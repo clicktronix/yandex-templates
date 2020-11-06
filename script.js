493f098894f969fba61a1ed7781de9a6d81439ac
clicktronix <s28.morgan@gmail.com>
2020-11-06 22:25:04 +0600

    feet: initial todos rendering

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const initialTodos = [
   'Разобраться в замыканиях',
   'Решить задачу на Codewars',
 ]
+
+const todoList = document.querySelector(".todo-list")
+
+initialTodos.forEach(todoTitle => {
+  const templateTodo = `
+    <article class="todo-list__item todo">
+      <h5 class="todo__title todo__title_theme_dark">${todoTitle}</h5>
+
+      <div class="todo__controls">
+        <button type="button" class="todo__control control control_edit" />
+        <button type="button" class="todo__control control control_duplicate" />
+        <button type="button" class="todo__control control control_remove" />
+      </div>
+    </article>
+  `
+
+  todoList.insertAdjacentHTML("beforeend", templateTodo)
+})
