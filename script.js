38c61da34c10e909cfd8caddf16efa27b1e6eafc
clicktronix <s28.morgan@gmail.com>
2020-11-07 00:01:02 +0600

    refactor: deleted extra handlers

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ const initialTodos = [
 const todoList = document.querySelector(".todo-list")
 const todoForm = document.querySelector(".add-todo-form")
 
-
-
 function addTodo(todoTitle) {
   const todoElement = document.querySelector(".todo-template").content.cloneNode(true)
 
@@ -44,32 +42,6 @@ function addTodo(todoTitle) {
 
   todoList.append(todoElement)
 }
-
-
-
-// document.addEventListener("click", event => {
-//   const target = event.target
-
-//   if (target.classList.contains("control_remove")) {
-//     const todo = event.target.closest(".todo")
-
-//     todo.remove()
-//   } else if (target.classList.contains("control_edit")) {
-//     const todo = event.target.closest(".todo")
-//     const todoTitle = todo.querySelector(".todo__title").textContent
-
-//     todoForm.querySelector(".add-todo-form__field_input").value = todoTitle
-
-//     todo.classList.add("is-edit")
-//   } else if (target.classList.contains("control_duplicate")) {
-//     const todo = event.target.closest(".todo")
-//     const duplicatedTitle = todo.querySelector(".todo__title").textContent
-
-//     addTodo(duplicatedTitle)
-//   }
-// })
-
-
 
 todoForm.addEventListener("submit", event => {
   event.preventDefault()
